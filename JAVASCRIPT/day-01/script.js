a2b6eda6c7aeccfd8768b61191af59629ffed3c8
Srushti Patil <Srushti200>
2025-03-07 10:35:49 +0530

    Variable initialisatin and declaration

---
+++ JAVASCRIPT/day-01/script.js
@@ -0,0 +1,15 @@
+// console.log("hii");
+
+// var city;
+
+// var name;
+
+// var age;
+// console.log(age);
+
+// variable inatialization and declaration
+// var age = 35;
+
+let age = 35;
+const city = "Pune";
+
